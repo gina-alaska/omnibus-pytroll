32433e039417d2544ab95afbd4814eeb16d624fe
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-11-11 14:59:52 -0900

    pyresample

---
+++ config/projects/pyresample.rb
@@ -0,0 +1,39 @@
+
+name "pytroll"
+maintainer "Scott Macfarlane"
+homepage "http://www.gina.alaska.edu"
+
+replaces        "pytroll"
+install_path    "/opt/pytroll"
+build_version   Omnibus::BuildVersion.new.semver
+build_iteration 1
+
+# creates required build directories
+dependency "preparation"
+
+# pytroll dependencies/components
+# dependency "somedep"
+dependency "readline"
+dependency "zlib"
+dependency "ncurses"
+dependency "openssl"
+dependency "Python"
+dependency "numpy"
+dependency "hdf5"
+dependency "h5py"
+dependency "freetype"
+dependency "libpng"
+dependency "libtiff"
+dependency "proj"
+dependency "libgeotiff"
+dependency "matplotlib"
+dependency "geos"
+dependency "basemap"
+dependency "setuptools"
+dependency "pyresample"
+
+# version manifest file
+dependency "version-manifest"
+
+exclude "\.git*"
+exclude "bundler\/git"
